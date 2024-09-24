b825405b9a031ba322ad3879d1ab13058a1fec36
staccblox <67884277+staccblox@users.noreply.github.com>
2024-09-24 13:47:49 -0400

    Update operations.cbl

--- operations.cbl
+++ operations.cbl
@@ -2,5 +2,5 @@ IDENTIFICATION DIVISION.
 PROGRAM-ID. IDSAMPLE.
 ENVIRONMENT DIVISION.
 PROCEDURE DIVISION.
-    DISPLAY 'HELLO WORLD'.
+    DISPLAY 'Managing the inner workings of your business so it runs as efficiently as possible.'.
     STOP RUN.
